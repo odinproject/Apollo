13d0d9b9de6d92089fb6946480a7e1aa0f8b7e4c
MysteriousArtifact <mysteriousartifact@gmail.com>
2014-03-05 20:36:40 -0700

    More Comments

--- src/apollo/learning/ChordWeights.java
+++ src/apollo/learning/ChordWeights.java
@@ -8,14 +8,28 @@ import apollo.chordbase.Chord;
  */
 public class ChordWeights {
     
+    // The actual weights for all possible chord transitions
+    // this contains (among other things) a four-dimensional array called weights
+    // [A][B][C][D]
+    // "A" represents which predictor we are looking at: 0 = Energy, 1 = Tension
+    // "B" represents the start chord type (0 = Major, 1 = Minor, etc...)
+    // "C" represents the pitch shift. This ranges from 0 to 22 (which represents
+    //   the shift ranges -11 through 11. 0 = -11, 1 = -10, w = -9...
+    // "D" represents the end chord type (0 = Major, 2 = Minor, etc...)
+    //   The value in the "D" entry is the average energy (or intensity) for that
+    //   chord transition
     private Double[][][][] transitionScores;
     
+    // how much to weight the effect of transitionScores
     private double comprehensiveWeight;
+    // how much to weight the effect of individual transitions
     private double typeTransitionWeight;
+    // how much to weight the effect of raw chord types
     private double rawTypeWeight;
     
     public ChordWeights()
     {
+        // right now we only care about the transitionScores
         comprehensiveWeight = 1.0;
         typeTransitionWeight = 0.0;
         rawTypeWeight = 0.0;
@@ -60,6 +74,14 @@ public class ChordWeights {
         }
     }
     
+    /**
+     * Imports a final weight directly into the transitionScores
+     * @param i the classifier index (0=energy, 1=tension)
+     * @param j the starting chord type (0=Major, 1=minor...)
+     * @param k the relative pitch shift index (0=-11, 1=-10... 22=11)
+     * @param l the ending chord type (0=Major, 1=minor...)
+     * @param score the intended intensity or energy score for this transition
+     */
     public void importWeight(int i, int j, int k, int l, double score)
     {
         transitionScores[i][j][k][l] = score;
